8dbd5ad27f32c1c86658c00a4ffc41ba7f44c3c2
Md Antor <md.antor1155@gmail.com>
2022-08-31 20:48:32 +0600

    solution of the bug of removing active class form circle after .5s is instantly go out and come in

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
+
 // design to table circle animation for hovering over the file 
 
 document.getElementById("file").addEventListener("mouseover", function () {
-    document.querySelectorAll('.floatingCircle').forEach(circle => circle.style.opacity="1");
+    document.querySelectorAll('.floatingCircle').forEach(circle => {
+        circle.style.opacity = "1";
+
+
+        // this part solve the bug : if mouse out of file and comes back before 500ms then it add active section then remove it before the removing function was waiting for 500 ms to remove any active classname add 
+        if (!circle.matches(".active")) {
+            circle.classList.add("active");
+        }
+        else {
+            setTimeout(() => {
+                circle.classList.add("active");
+            }, 500);
+        }
+    });
 })
 
+
 document.getElementById("file").addEventListener("mouseout", function () {
-    document.querySelectorAll('.floatingCircle').forEach(circle => circle.style.opacity="0");
+    document.querySelectorAll('.floatingCircle').forEach(circle => {
+        circle.style.opacity = "0";
+        setTimeout(() => {
+            circle.classList.remove("active");
+        }, 500);
+
+    });
 })
 
