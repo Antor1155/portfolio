3fd95724e28a6458f176630921417dfa1683737f
Md Antor <md.antor1155@gmail.com>
2022-09-06 16:30:11 +0600

    eye movement when mouse moved done

--- main.js
+++ main.js
@@ -85,9 +85,13 @@ document.getElementById("previousBtn").addEventListener("click", function(){
 
 // eye Animation 
 document.querySelector('body').addEventListener('mousemove', function(){
+    console.log("mouse moving")
     let eye = document.querySelectorAll(".eye");
     eye.forEach(function(eye){
         let x = (eye.getBoundingClientRect().left) + (eye.clientWidth/2);
-        let y = (eye.getBoundingClientRect().left) + (eye.clientHeight/2);
+        let y = (eye.getBoundingClientRect().top) + (eye.clientHeight/2);
+        let radian = Math.atan2(event.pageX - x, event.pageY - y);
+        let rot = (radian * (180/Math.PI)* -1) + 270;
+        eye.style.transform = "rotate(" + rot +"deg)"
     })
 })
